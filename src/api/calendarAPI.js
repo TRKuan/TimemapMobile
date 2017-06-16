d3280519af67cd3a2976b024f37224ae37fed0dd
TR <tianruei.kuan@gmail.com>
2017-06-16 20:59:42 +0800

    add api

---
+++ src/api/calendarAPI.js
@@ -0,0 +1,118 @@
+import moment from 'moment';
+
+const baseUrl = 'http://timemap-server.us-west-2.elasticbeanstalk.com/api';
+
+export function addEvent(event) {
+    let url = `${baseUrl}/events`;
+
+    console.log(`Making POST request to: ${url}`);
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            ...event
+        })
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
+
+export function setEvent(event, eventID) {
+    let url = `${baseUrl}/events/${eventID}`;
+
+    console.log(`Making POST request to: ${url}`);
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            ...event
+        })
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
+
+export function getDay(userId, year, month, day) {
+    let startTime = moment({year, month:month-1, day}).format('YYYY-MM-DD ZZ');
+    let endTime = moment({year, month:month-1, day});
+    endTime = endTime.date(day+1).format('YYYY-MM-DD ZZ');
+    let url = `${baseUrl}/day?userId=${userId}&startTime=${encodeURIComponent(startTime)}&endTime=${encodeURIComponent(endTime)}`;
+
+    console.log(`Making GET request to: ${url}`);
+
+    return fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
+
+export function getMonth(userId, year, month) {
+    let zone = moment().format('ZZ');
+    let url = `${baseUrl}/month?userId=${userId}&year=${year}&month=${month}&zone=${encodeURIComponent(zone)}`;
+
+    console.log(`Making GET request to: ${url}`);
+
+    return fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
+
+export function getNextEvent(userId) {
+    let url = `${baseUrl}/nextevent?userId=${userId}`;
+
+    console.log(`Making GET request to: ${url}`);
+
+    return fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+        return res.data[0];
+    });
+}
+
+export function getEvent(userId) {
+    let url = `${baseUrl}/events?userId=${userId}`;
+
+    console.log(`Making GET request to: ${url}`);
+
+    return fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
---
+++ src/api/mapboxAPI.js
@@ -0,0 +1,35 @@
+const baseUrl = 'http://timemap-server.us-west-2.elasticbeanstalk.com/api';
+export function getDirection(coords1, coords2, profile, accessToken){
+    const directionURL = `https://api.mapbox.com/directions/v5/mapbox/${profile}/${coords1.lng},${coords1.lat};${coords2.lng},${coords2.lat}.json?access_token=${accessToken}`;
+    console.log(`Making GET request to: ${directionURL}`);
+    return fetch(directionURL, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if(res.data.code !== "Ok")throw Error("Mapbox Direaction error");
+        return {
+            duration: res.data.routes[0].duration,
+            distance: res.data.routes[0].distance
+        };
+    }).catch((err) => {
+        throw err;
+    });
+}
+
+export function getAccessToken() {
+    let url = `${baseUrl}/accesstoken`;
+
+    console.log(`Making GET request to: ${url}`);
+
+    return fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
+    });
+}
