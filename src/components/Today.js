fbe4d65898bdbfe0d4b80e4a57cbb223f19f85c7
104060011 <twtnyk@gmail.com>
2017-06-22 10:30:48 +0800

    fix today no next event

--- src/components/Today.js
+++ src/components/Today.js
@@ -114,13 +114,18 @@ class Today extends Component {
                     <Text style={styles.todayDateOfWeekText}>{moment().format('dddd')}</Text>
                 </View>
                 <View style={[styles.info, {backgroundColor: this.state.infoBackground}]}>
+                {
+                  this.props.nextEvent?
                   <View>
                     <Text style={[styles.textLight,{ marginBottom: 4, fontSize: size*1}]}><Icon name="bullseye" size={size} color={theme.themeColorLight} />&nbsp;&nbsp; Next Event{"\n"}</Text>
                     <Text style={[styles.textLight, {marginBottom: size, fontSize: size*2, paddingLeft:size*1.8, borderLeftWidth:7, borderColor:"rgb(56, 237, 123)"}]}>{this.props.title}</Text>
                     <Text style={[styles.textLight, {fontSize: size*0.9, margin:2, color: '#ccc'}]}><Icon name="bell-o" iconStyle={{marginRight: 20}} size={size} color={'#ddd'/*theme.themeColorLight*/} />&nbsp;&nbsp; Should leave {moment.duration(this.props.leaveTime, 'seconds').humanize(true)}</Text>
                     <Text style={[styles.textLight, {fontSize: size*0.9, margin:2, color: '#ccc'}]}><Icon name="clock-o" size={size} color={'#ddd'/*theme.themeColorLight*/} />&nbsp;&nbsp; {moment(this.props.startTs).format('MMM D, YYYY')}</Text>
                     <Text style={[styles.textLight, {fontSize: size*0.9, margin:2, color: '#ccc', paddingLeft:size/4}]}><Icon name="map-marker" size={size} color={'#ccc'/*theme.themeColorLight*/} />&nbsp;&nbsp;&nbsp; {this.props.location}</Text>
-                </View>
+                  </View>:
+                  <Text style={{fontSize: size*2, color: theme.themeColorLight}}>No Next Event</Text>
+                }
+
               </View>
             </View>
             <View style={{height:"45%", width:"100%"}}>
